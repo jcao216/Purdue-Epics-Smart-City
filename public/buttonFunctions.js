6bbaaa408eaa578caa4888ba7341e3144e63ed1f
jcao216 <35784684+jcao216@users.noreply.github.com>
2018-10-17 13:54:30 -0400

    Initial implementation of date sorting.

---
+++ public/buttonFunctions.js
@@ -0,0 +1,8 @@
+function dateBtn(day, month, year)
+{
+while()
+{
+	document.getElementByID("dateBtn").innerHTML = "Date [by newest]";
+}
+	document.getElementByID("dateBtn").innerHTML = "Date [by oldest]";
+}
